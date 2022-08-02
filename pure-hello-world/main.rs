12695d0695faf3ee29f8b5647fa4ca62b1b8dae2
pontakornth <pontakorn61@gmail.com>
2022-08-02 11:31:02 +0700

    feat: Hello World without Cargo

---
+++ pure-hello-world/main.rs
@@ -0,0 +1,10 @@
+/* This is pure rust project.
+ * I don't use cargo here. You may have some problems with editors
+ * or IDEs if you try to edit this file.
+ *
+ * Compile using `rustc main.rs`
+ */
+
+fn main() {
+    println!("Hello, World");
+}
