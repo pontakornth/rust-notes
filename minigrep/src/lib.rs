65cd507158a0cf8a74bdac0fc63d3a263e9d0a66
pontakornth <pontakorn61@gmail.com>
2022-08-05 14:13:29 +0700

    feat: Minigrep 12.3

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,26 @@
+use std::{error::Error, fs};
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        // It's OK to clone a few string. We will know how to do it better when you know about
+        // iterator.
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("With text:\n{}", contents);
+    Ok(())
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,22 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
+
+use minigrep::{run, Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     // 0 is the program name.
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
