69b99a031405cf169458a68fdc368a652868abc4
pontakornth <pontakorn61@gmail.com>
2022-08-05 14:57:13 +0700

    feat: Minigrep 12.5

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -13,8 +13,6 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
     if let Err(e) = run(config) {
         println!("Application error: {}", e);
         process::exit(1);
