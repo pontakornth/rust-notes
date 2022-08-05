f526405e0bf0387463ed85e19d1feeca5170f8ff
pontakornth <pontakorn61@gmail.com>
2022-08-05 15:05:21 +0700

    feat: Minigrep 12.6

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -10,11 +10,11 @@ fn main() {
 
     // 0 is the program name.
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
